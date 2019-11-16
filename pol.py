d748a73447273e3f550cfef93c68c3fe18139cec
mr2dolphin <pozitiv.danka@gmail.com>
2019-11-16 14:22:15 +0500

    Update pol.py

--- pol.py
+++ pol.py
@@ -29,3 +29,33 @@ class Stack(list):
 			t = self[-2]
 			self[-3] = self[-3]
 			self[-3] = t
+
+def to_pol(s):
+	res = ''
+	s = '( ' + s +' )'
+	ops = Stack()
+	prs = {'+' : 2, '-' : 2, '*' : 3, '/' : 3, '%' : 3, '(' : 1}
+	for i in s.split():
+		if i[-1].isdigit():
+			res += i + ' '
+		elif i == '(':
+			ops.put(i)
+		elif i == ')':
+			t = ops.pull()
+			while t != '(':
+				res += t + ' '
+				t = ops.pull()
+		else:
+			try:
+				t = ops[-1]
+				while prs[t]>=prs[i] and t != '(':
+					res += ops.pull() + ' '
+					t = ops[-1]
+				ops.put(i)
+			except:
+				pass
+	return res
+print(to_pol('( ( 2 + 4 ) * 7 - 3 * 5 ) / ( 2 + 7 ) * 4'))
+print(to_pol('( 2 + 3 ) * 5'))
+print(to_pol('3 * 2 - 5'))
+print(to_pol(input()))
